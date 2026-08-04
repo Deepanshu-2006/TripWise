ba32bc9455ee3d8f1c41e213965067a156cc78c2
Deepanshu Khatri <deepanshukhatri20061972@gmail.com>
2026-08-05 03:17:36 +0530

    feat(homepage): update homepage animations, sections, and layout transitions

--- app/page.js
+++ app/page.js
@@ -5,28 +5,17 @@ import Hero from "./components/Hero";
 import RealTimeAdjuster from "./components/Live Demo";
 import PassportGrid from "./components/PassportGrid";
 import FAQAndMarqueeCTA from "./components/FAQAndMarqueeCTA";
-import ColorMorphWrapper from "./components/ColorMorphWrapper";
 
 export default function Home() {
   return (
-    <ColorMorphWrapper>
+    <div>
       <Header />
-      <div data-color="#FEF8F5">
-        <Hero />
-      </div>
-      <div data-color="#F0FDFA">
-        <FeaturesSelection />
-      </div>
-      <div data-color="#FFF1EB">
-        <RealTimeAdjuster />
-      </div>
-      <div data-color="#F9FAFB">
-        <Destination />
-      </div>
-      <div data-color="#FFF8F5">
-        <PassportGrid />
-      </div>
-      <FAQAndMarqueeCTA /> {/* Already has internal data-color tags */}
-    </ColorMorphWrapper>
+      <Hero />
+      <FeaturesSelection />
+      <RealTimeAdjuster />
+      <Destination />
+      <PassportGrid />
+      <FAQAndMarqueeCTA />
+    </div>
   );
 }
